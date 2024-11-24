b745b572de319e69d02acea3f0302209479aa7e5
rasibnadeemdev <rasibnadeemdev@gmail.com>
2024-11-24 16:38:55 +0500

    task 2 done

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,1 +1,85 @@
 // Todo: Implement the User Service Logic to be used in the Resolver - Following MVR Model (Model View Resolver)
+import { Error } from "mongoose";
+
+import User from "./user.schema";
+import { IUser } from "./user.schema";
+
+class UserService {
+  static async findAllUsers(): Promise<IUser[]> {
+    try {
+      const users = await User.find();
+      return users;
+    } catch (error) {
+      throw new Error("Failed to fetch users");
+    }
+  }
+
+  static async findUserById(userId: string): Promise<IUser | null> {
+    try {
+      const user = await User.findById(userId);
+      return user;
+    } catch (error) {
+      throw new Error("User not found");
+    }
+  }
+
+  static async updateUser(
+    userId: string,
+    updates: Partial<
+      Pick<IUser, "email" | "username" | "password" | "isAdmin" | "role">
+    >
+  ): Promise<IUser | null> {
+    try {
+      if (Object.keys(updates).length === 0) {
+        throw new Error("No updates provided");
+      }
+
+      const updatedUser = await User.findByIdAndUpdate(userId, updates, {
+        new: true,
+      });
+
+      if (!updatedUser) {
+        throw new Error("User not found");
+      }
+
+      return updatedUser;
+    } catch (error: any) {
+      throw new Error(`Failed to update user: ${error.message}`);
+    }
+  }
+
+  static async deleteUser(userId: string): Promise<IUser | null> {
+    try {
+      const deletedUser = await User.findByIdAndDelete(userId);
+      return deletedUser;
+    } catch (error) {
+      throw new Error("Failed to delete user");
+    }
+  }
+
+  static async findUserByEmail(email: string): Promise<IUser | null> {
+    try {
+      const user = await User.findOne({ email });
+      return user;
+    } catch (error) {
+      throw new Error("Failed to fetch user by email");
+    }
+  }
+  static async createUser(userData: {
+    email: string;
+    username: string;
+    password: string;
+    role: string;
+    isAdmin: boolean;
+  }): Promise<IUser> {
+    try {
+      const newUser = new User(userData);
+      await newUser.save();
+      return newUser;
+    } catch (error) {
+      throw new Error("Failed to create user");
+    }
+  }
+}
+
+export default UserService;
